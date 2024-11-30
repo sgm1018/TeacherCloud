431504a17c45fb7070152f6083207a65c43f52f4
Sergio González Martínez <1018sgm1018@gmail.com>
2024-11-30 19:13:34 +0100

    Feat: init proyecto spring

---
+++ src/main/java/com/teachercloud/TeachercloudApplication.java
@@ -0,0 +1,13 @@
+package com.teachercloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeachercloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeachercloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teachercloud/TeachercloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teachercloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachercloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
